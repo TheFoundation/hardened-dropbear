805243560d1d1e30cb119cbfb4c9e1c9c496e4f0
Benjamin Freitag <12718580+benchonaut@users.noreply.github.com>
2023-04-02 15:43:17 +0000

    .

--- _0.test.build.sh
+++ _0.test.build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-BUILD_TARGET_PLATFORMS="linux/amd64,linux/arm64,linux/arm/v7"
+[[ -z "$BUILD_TARGET_PLATFORMS" ]] && BUILD_TARGET_PLATFORMS="linux/amd64,linux/arm64,linux/arm/v7"
 
 _platform_tag() { echo "$1"|sed 's~/~_~g' ;};
 _oneline()               { tr -d '\n' ; } ;
@@ -40,8 +40,8 @@ mkdir build
 buildstring=build
 DFILENAME=$startdir/Dockerfile.alpine
 echo "singlearch-build for "$BUILDARCH
-echo time docker buildx build  --output=type=registry,push=false   --pull --progress plain --network=host --memory-swap -1 --memory 1024 --platform=${BUILDARCH} --cache-from ${IMAGETAG} -t  ${IMAGETAG} $buildstring -f "${DFILENAME}" 
-time docker buildx build  --output=type=registry,push=true --push   --pull --progress plain --network=host --memory-swap -1 --memory 1024 --platform=${BUILDARCH} --cache-from ${IMAGETAG} -t  ${IMAGETAG} $buildstring -f "${DFILENAME}" 
+echo time docker buildx build  --output=type=registry,push=true --push   --pull --progress plain --network=host --memory-swap -1 --memory 1024 --platform=${BUILDARCH} --cache-to ${IMAGETAG}_${TARGETARCH}_buildcache  --cache-from ${IMAGETAG}_${TARGETARCH}_buildcache -t  ${IMAGETAG} $buildstring -f "${DFILENAME}" 
+time docker buildx build  --output=type=registry,push=true --push   --pull --progress plain --network=host --memory-swap -1 --memory 1024 --platform=${BUILDARCH} --cache-to ${IMAGETAG}_${TARGETARCH}_buildcache  --cache-from ${IMAGETAG}_${TARGETARCH}_buildcache -t  ${IMAGETAG} $buildstring -f "${DFILENAME}" 
 
 
 done
@@ -49,3 +49,30 @@ done
 
 #time docker buildx build  --output=type=registry,push=true  --push  --pull --progress plain --network=host --memory-swap -1 --memory 1024 --platform=${TARGETARCH} --cache-from ${IMAGETAG} -t  ${IMAGETAG} $buildstring -f "${DFILENAME}" 
 
+for BUILDARCH in $(echo $BUILD_TARGET_PLATFORMS |sed 's/,/ /g');do
+TARGETARCH=$(_platform_tag $BUILDARCH  )
+TARGETDIR=builds/$TARGETARCH
+echo "building to "$TARGETDIR
+mkdir -p "$TARGETDIR"
+startdir=$(pwd)
+cd "$TARGETDIR"
+mkdir build
+(
+    cd build
+    cp ${startdir}/build-bear.sh . -v
+    test -e ccache.tgz && rm ccache.tgz
+    docker export $(docker create --name cicache $IMAGETAG /bin/false ) |tar xv ccache.tgz ;docker rm cicache
+    test -e ccache.tgz || ( mkdir .tmpempty ;echo 123 .tmpempty/file;tar cvzf ccache.tgz .tmpempty )
+    test -e dropbear-src || git clone https://github.com/mkj/dropbear.git dropbear-src
+    test -e .tmpempty && rm -rf .tmpempty
+)
+
+buildstring=build
+DFILENAME=$startdir/Dockerfile.alpine
+echo "build for "$BUILDARCH
+echo time docker buildx build  --output=type=registry,push=true --push   --pull --progress plain --network=host --memory-swap -1 --memory 1024 --platform=${BUILDARCH} --cache-to ${IMAGETAG}_${TARGETARCH}_buildcache  --cache-from ${IMAGETAG}_${TARGETARCH}_buildcache -t  ${IMAGETAG} $buildstring -f "${DFILENAME}" 
+time docker buildx build  --output=type=registry,push=true --push   --pull --progress plain --network=host --memory-swap -1 --memory 1024 --platform=${BUILDARCH} --cache-to ${IMAGETAG}_${TARGETARCH}_buildcache  --cache-from ${IMAGETAG}_${TARGETARCH}_buildcache -t  ${IMAGETAG} $buildstring -f "${DFILENAME}" &
+
+
+done
+wait
